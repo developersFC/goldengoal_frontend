2337b6409a77c7562062b793c16314f56b008db3
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-07-11 20:06:20 +0300

    addingDay1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import FavForm from './component/FavTeam'
-
+import SlideShow from './component/SlideShow'
 export class App extends Component {
   render() {
     return (
       <div>
+        <SlideShow/>
+
         <FavForm/>
       </div>
     )
