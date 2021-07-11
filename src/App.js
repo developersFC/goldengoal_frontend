eae06d597ba725bce7c9323a78bed979ccd82d97
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-07-11 20:01:20 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
-import FavForm from './component/FavTeam'
+import React, { Component } from 'react';
+import FavForm from './component/FavTeam';
+import Footer from './component/Footer';
+import Header from './component/Header';
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <FavForm/>
+        <Header />
+        <FavForm />
+        <Footer />
       </div>
     )
   }
