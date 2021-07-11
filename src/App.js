1f04d2fe7d3d85fcc964afb747f0babf21cb4c44
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-07-12 00:21:12 +0300

    addingCss

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import FavForm from './component/FavTeam';
 import Footer from './component/Footer';
 import Header from './component/Header';
 import SlideShow from './component/SlideShow'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+import './App.css'
 
 export class App extends Component {
+  
   render() {
     return (
-      <div>
+      <div className="div">
 
         <Header />
     
