728fe26f2de314f2450aac4710c841f8b14748d8
qasem-moh <2171246@s.aou.edu.jo>
2021-07-13 13:49:51 +0300

    hero

--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -11,6 +11,13 @@ const FormSignup = ({ submitForm }) => {
 
   return (
     <div className="form-content-right">
+      <div className="descForm">
+        {" "}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+        recusandae aliquid sunt, a dolorem ad corrupti voluptas fuga deserunt
+        animi minima velit quam! Quibusdam maiores laborum sunt, ab culpa
+        quidem.
+      </div>
       <form onSubmit={handleSubmit} className="form" noValidate>
         <div className="form-inputs">
           <label className="form-label">Enter the country code </label>
@@ -26,7 +33,13 @@ const FormSignup = ({ submitForm }) => {
         </div>
 
         <select
-          style={{ width: "125px", textAlign: "center", fontSize: "20px" ,width:'348px',height:'40px'}}
+          style={{
+            width: "125px",
+            textAlign: "center",
+            fontSize: "20px",
+            width: "348px",
+            height: "40px",
+          }}
         >
           <option value="jo">jo</option>
           <option value="sy">sy</option>
