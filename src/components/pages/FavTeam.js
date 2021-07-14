8960184faedccda7effebe445841ad1260482aba
thaerbraizat <thaerbraizat13@gmail.com>
2021-07-14 20:03:20 +0300

    gg

--- src/components/pages/FavTeam.js
+++ src/components/pages/FavTeam.js
@@ -49,6 +49,15 @@ class FavTeam extends Component {
   };
   render() {
     return (
+      <>
+      <div className="hero-container">
+      {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
+      <img
+        src="https://www.photoshop-library.com/wp-content/uploads/edd/2020/09/%D8%B5%D9%88%D8%B1%D8%A9-%D8%AE%D9%84%D9%81%D9%8A%D8%A9-%D9%85%D9%84%D8%B9%D8%A8-%D9%83%D8%B1%D8%A9-%D9%82%D8%AF%D9%85-JBG.jpg"
+        alt=""
+        id="coverimg"
+      />
+   
       <div>
         <Card
           style={{
@@ -123,12 +132,15 @@ class FavTeam extends Component {
             <br></br>
             <br></br>
             </Modal.Body>
-            <Modal.Footer>
+            {/* <Modal.Footer>
               <Button    variant="outlined" color="primary" onClick={() => {this.handleModal()}}>
                 Close
               </Button>
 
-            </Modal.Footer>
+            </Modal.Footer> */}
+            <button className="botClose1" onClick={() => {this.handleModal()}}>
+            Close
+            </button>
           </Modal>
 
 
@@ -143,14 +155,15 @@ class FavTeam extends Component {
            
               <Table className="table" responsive="sm">
           <div className='desForm'>Football / Soccer Club World Ranking</div>
-         
+        
             <tr>
               <th>#</th>
               <th>Table heading</th>
               <th>Table heading</th>
               <th>Table heading</th>
             </tr>
-          
+         
+    
             <tr>
               <td>1</td>
               <td>Table cell</td>
@@ -167,12 +180,15 @@ class FavTeam extends Component {
     
 
             </Modal.Body>
-            <Modal.Footer>
-              <Button    variant="outlined" color="primary" onClick={() => {this.handleRanking()}}>
+            {/* <Modal.Footer>
+              <Button className="botClose"   variant="outlined" color="primary" onClick={() => {this.handleRanking()}}>
                 Close
               </Button>
 
-            </Modal.Footer>
+            </Modal.Footer> */}
+            <button className="botClose" onClick={() => {this.handleRanking()}}>
+            Close
+            </button>
           </Modal>
 
 
@@ -180,16 +196,13 @@ class FavTeam extends Component {
         </Card>
         
       </div>
+     
+    </div>
+    
+      {/* <Footer /> */}
+     </>
     )
   }
 }
 
-export default FavTeam
-
-
-
-
-
-
-
-
+export default FavTeam;
