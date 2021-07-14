e98be47a59ec7843c91f2f88e40bda16cd6d4099
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-07-14 20:55:48 +0300

    LAST ABOUT CSS

--- src/components/pages/Compare.js
+++ src/components/pages/Compare.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 import { Card, ListGroupItem, ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 import { MDBIcon, MDBContainer } from 'mdbreact';
-
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
+import Footer from '../Footer';
 import './Compare.css'
+// import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
+
 
 export class Compare extends Component {
   render() {
     return (
+      <div>
       <div className="aboutUsDiv">
 
         <div class="all">
@@ -21,21 +20,19 @@ export class Compare extends Component {
               <Card.Body>
                 <Card.Title>Mohammad Lababneh</Card.Title>
                 <Card.Text>
-                  Some quick example the bulk of
-                  the card's content.
-                </Card.Text>
+                Mechanical Engineer graduated from (JUST university)                </Card.Text>
               </Card.Body>
               <ListGroup className="list-group-flush"><ListGroupItem>Software Developer</ListGroupItem></ListGroup>
               <Card.Body>
                 <MDBContainer className="MDBContainer">
 
-                  <a href="https://github.com/mohammad-lababneh" className='a' className="fb-ic mr-3" target="_blank">
+                  <a href="https://github.com/mohammad-lababneh" className='a' className="fb-ic mr-3"  target="_blank">
                     <MDBIcon className="MDBIcon" fab icon="facebook-f" />
                   </a>
                   <a href="https://github.com/mohammad-lababneh" className='a' className="tw-ic mr-3" target="_blank">
                     <MDBIcon className="MDBIcon" fab icon="twitter" />
                   </a>
-                  <a href="https://github.com/mohammad-lababneh" className='a' className="li-ic mr-3" target="_blank">
+                  <a href="https://www.linkedin.com/in/mohammad-lababneh-74755b1b0" className='a' className="li-ic mr-3" target="_blank">
                     <MDBIcon className="MDBIcon" fab icon="linkedin-in" />
                   </a>
                   <a href="https://github.com/mohammad-lababneh" className='a' className="git-ic mr-3" target="_blank">
@@ -47,10 +44,17 @@ export class Compare extends Component {
             </Card>
 
           </div>
-          </div>
+        </div>
 
 
-          <div class="all">
+
+
+
+
+
+
+
+        <div class="all">
 
           <Card className="cards">
             <Card.Img variant="top" src="https://ca.slack-edge.com/TNGRRLUMA-U01RY3TCY11-7c15bdd95f8d-512" className='img' />
@@ -59,35 +63,36 @@ export class Compare extends Component {
             <Card.Body>
               <Card.Title >Thaer Braizat</Card.Title>
               <Card.Text>
-                Some quick example the bulk of
-                the card's content.
+               computer engineer.
+               <br/> 
+               likes online games, all sport games, and interested in web development
               </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush"><ListGroupItem>Software Developer</ListGroupItem></ListGroup>
             <Card.Body>
               <MDBContainer className="MDBContainer">
                 <a href="#!" className="fb-ic mr-3">
-                  <MDBIcon className="MDBIcon" fab icon="facebook-f" />
+                  <MDBIcon className="MDBIcon" fab icon="facebook-f" target="_blank" />
                 </a>
-                <a href="#!" className="tw-ic mr-3">
+                <a href="#!" className="tw-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="twitter" />
                 </a>
-                <a href="#!" className="li-ic mr-3">
+                <a href="https://www.linkedin.com/in/thaer-braizat-a7b513200/" className="li-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="linkedin-in" />
                 </a>
-                <a href="#!" className="git-ic mr-3">
+                <a href="https://github.com/thaerbraizat" className="git-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="github" />
                 </a>
               </MDBContainer>
 
             </Card.Body>
           </Card>
-          </div>
+        </div>
 
 
 
 
-          <div class="all">
+        <div class="all">
 
           <Card className="cards">
             <Card.Img variant="top" src="https://ca.slack-edge.com/TNGRRLUMA-U01UM9UPGQY-ddd5b98f8cc8-512" className='img' />
@@ -96,33 +101,32 @@ export class Compare extends Component {
             <Card.Body>
               <Card.Title>Qasem Mohammad </Card.Title>
               <Card.Text>
-                Some quick example the bulk of
-                the card's content.
+              Media and press from Arab Open University (AOU)
               </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush"><ListGroupItem>Software Developer</ListGroupItem></ListGroup>
             <Card.Body>
               <MDBContainer className="MDBContainer">
-                <a href="#!" className="fb-ic mr-3">
+                <a href="#!" className="fb-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="facebook-f" />
                 </a>
-                <a href="#!" className="tw-ic mr-3">
+                <a href="#!" className="tw-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="twitter" />
                 </a>
-                <a href="#!" className="li-ic mr-3">
+                <a href="#!" className="li-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="linkedin-in" />
                 </a>
-                <a href="#!" className="git-ic mr-3">
+                <a href="#!" className="git-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="github" />
                 </a>
               </MDBContainer>
 
             </Card.Body>
           </Card>
-          </div>
+        </div>
 
 
-          <div class="all">
+        <div class="all">
 
           <Card className="cards">
             <Card.Img variant="top" src="https://ca.slack-edge.com/TNGRRLUMA-U01SAFN4JUR-99c2bd50d725-512" className='img' />
@@ -138,50 +142,52 @@ export class Compare extends Component {
             <ListGroup className="list-group-flush"><ListGroupItem>Software Developer</ListGroupItem></ListGroup>
             <Card.Body>
               <MDBContainer className="MDBContainer">
-                <a href="#!" className="fb-ic mr-3">
+                <a href="#!" className="fb-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="facebook-f" />
                 </a>
-                <a href="#!" className="tw-ic mr-3">
+                <a href="#!" className="tw-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="twitter" />
                 </a>
-                <a href="#!" className="li-ic mr-3">
+                <a href="#!" className="li-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="linkedin-in" />
                 </a>
-                <a href="#!" className="git-ic mr-3">
+                <a href="#!" className="git-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="github" />
                 </a>
               </MDBContainer>
 
             </Card.Body>
           </Card>
-          </div>
+        </div>
 
 
 
-          <div class="all">
+        <div class="all">
 
           <Card className="cards">
             <Card.Img variant="top" src="https://ca.slack-edge.com/TNGRRLUMA-U01SV448ZB3-839684d6e331-512" className='img' />
             <Card.Body>
               <Card.Title>Nedal Alashqar</Card.Title>
               <Card.Text>
-                Some quick example the bulk of
-                the card's content.
+                Management Information Systems
               </Card.Text>
+              <Card.Text>
+                Zarqa University 
+            </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush"><ListGroupItem>Software Developer</ListGroupItem></ListGroup>
             <Card.Body>
               <MDBContainer className="MDBContainer">
-                <a href="#!" className="fb-ic mr-3">
+                <a href="#!" className="fb-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="facebook-f" />
                 </a>
-                <a href="#!" className="tw-ic mr-3">
+                <a href="#!" className="tw-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="twitter" />
                 </a>
-                <a href="#!" className="li-ic mr-3">
+                <a href="https://www.linkedin.com/in/nedal-alashqar-721b60153/" className="li-ic mr-3" target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="linkedin-in" />
                 </a>
-                <a href="#!" className="git-ic mr-3">
+                <a href="https://github.com/Nedalalashqar" className="git-ic mr-3 " target="_blank">
                   <MDBIcon className="MDBIcon" fab icon="github" />
                 </a>
               </MDBContainer>
@@ -189,12 +195,16 @@ export class Compare extends Component {
             </Card.Body>
           </Card>
         </div>
-        </div>
 
+      </div>
+      <Footer/>
 
+      </div>
 
     );
+
   }
+
 }
 
 export default Compare
